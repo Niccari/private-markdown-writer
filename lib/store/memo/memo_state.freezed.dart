10cbdc4f3d3f01334c6ad760d1520ea23f0494c1
Niccari <latieos93@gmail.com>
2023-05-08 20:48:53 +0900

    bump: v0.1.4

--- lib/store/memo/memo_state.freezed.dart
+++ lib/store/memo/memo_state.freezed.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'memo_state.dart';
 
@@ -163,6 +163,7 @@ class _$_MemoState implements _MemoState {
   final List<Memo> _memoList;
   @override
   List<Memo> get memoList {
+    if (_memoList is EqualUnmodifiableListView) return _memoList;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_memoList);
   }
@@ -170,6 +171,7 @@ class _$_MemoState implements _MemoState {
   final List<Memo> _deletedMemoList;
   @override
   List<Memo> get deletedMemoList {
+    if (_deletedMemoList is EqualUnmodifiableListView) return _deletedMemoList;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_deletedMemoList);
   }
